830c58e37f8b0fca7a329521604e5b801adf9353
Jacob <jacobgoldberg22@gmail.com>
2017-05-31 15:03:34 -0400

    Initial website upload

---
+++ scripts/js.js
@@ -0,0 +1,58 @@
+
+
+
+/*$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 400
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+}); */
+
+
+(function($) {
+$(document).ready(function() {
+    var navChildren = $("#nav li").children();
+    var aArray = [];
+    for (var i = 0; i < navChildren.length; i++) {
+        var aChild = navChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    }
+    $(window).scroll(function() {
+        var windowPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var docHeight = $(document).height();
+        for (var i = 0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var secPosition = $(theID).offset().top;
+            secPosition = secPosition - 135;
+            var divHeight = $(theID).height();
+            divHeight = divHeight + 90;
+            if (windowPos >= secPosition && windowPos < (secPosition + divHeight)) {
+                $("a[href='" + theID + "']").parent().addClass("active");
+            } else {
+                $("a[href='" + theID + "']").parent().removeClass("active");
+            }
+        }
+    });
+ 
+});
+})(jQuery);
